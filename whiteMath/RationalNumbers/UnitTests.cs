546b416bb0aa68df2c7f169c1fc3ce395842ca9b
Pavel Kabir <pkabir.work@gmail.com>
2016-01-17 10:19:24 +0300

    Add unit tests

---
+++ whiteMath/RationalNumbers/UnitTests.cs
@@ -0,0 +1,36 @@
+﻿#if (DEBUG)
+using NUnit.Framework;
+
+using IntRational = whiteMath.RationalNumbers.Rational<int, whiteMath.CalcInt>;
+using IntRationalCalculator = whiteMath.RationalNumbers.CalcRational<int, whiteMath.CalcInt>;
+
+namespace whiteMath.RationalNumbers
+{
+	[TestFixture]
+	public class UnitTests
+	{
+		private static readonly IntRationalCalculator calculator = new IntRationalCalculator();
+
+		[Test]
+		public void TestNegationWorksProperly(
+			[Random(-10, 10, 50)] int numerator,
+			[Random(-10, 10, 50)] int denominator
+		)
+		{
+			var number = new IntRational(numerator, denominator);
+			var negatedNumber = -number;
+
+			if (denominator == 0)
+			{
+				Assert.Pass();
+			}
+			else
+			{
+				Assert.That(
+					number + negatedNumber, 
+					Is.EqualTo(calculator.zero));
+			}
+		}
+	}
+}
+#endif
